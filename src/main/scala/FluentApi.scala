f76cc549b484d4d06ab34f3416218efbac2a5583
bernat-corommina <bernat.coromina@glovoapp.com>
2025-05-28 19:47:13 +0200

    Fluent api

--- src/main/scala/FluentApi.scala
+++ src/main/scala/FluentApi.scala
@@ -15,13 +15,13 @@ object Data {
     Box("box01", Dimensions(2,2,2), Product("ean01", 2, 3) :: Product("ean02", 3, 40) :: Nil),
     Box("box02", Dimensions(2,5,5), Product("ean04", 1, 54) :: Product("ean05", 30, 33) :: Product("ean06", 7, 1) :: Nil),
     Box("box03", Dimensions(6,3,2), Product("ean01", 2, 5) :: Product("ean02", 3, 10) :: Nil),
-    Box("box03", Dimensions(6,3,2), Nil),
+    Box("box04", Dimensions(6,3,2), Nil),
   )
 }
 
 object FluentApi {
   // 1- Filtrar cajas vacías
   // 2- Obtener una lista de productos únicos. Lista de ean's
-  // 3- Valor del stock de cada producto: Map[String, Int]
+  // 3- Valor del stock de cada caja: Map[String, Int]
   // 4- La caja de mas valor
 }
