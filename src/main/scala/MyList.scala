729d2052240b667531e95472e18888ff4a8dcbf5
bernat-corommina <bernat.coromina@glovoapp.com>
2025-06-04 09:45:46 +0200

    MyList append

--- src/main/scala/MyList.scala
+++ src/main/scala/MyList.scala
@@ -1,16 +1,27 @@
-import MyListSyntax.MyListOps
 
-abstract class MyList[+T]
+
+abstract class MyList[+T] {
+  def >>:[B>:T](elem: B): MyList[B] = MyCons(elem, this)
+}
 case object MyEmpty extends MyList[Nothing]
 case class MyCons[A](elem: A, tail: MyList[A]) extends MyList[A]
 
-object MyListSyntax {
-  implicit class MyListOps[A](v: A) {
-    def <<>>(tail: MyList[A]): MyList[A] = MyCons(v, tail)
-  }
-}
+
 
 object UsingMyList extends App{
   1 :: 2 :: 3 :: Nil
-  1 <<>> (2 <<>> (3 <<>> MyEmpty) )
+
+
+
+  List(1, 2, 3) match {
+    case ::(head, next) => ???
+    case Nil => ???
+  }
+
+
+ val l = 3 >>: MyEmpty
+  val a: MyList[Int] = 2 >>: 3 >>: MyEmpty
+  1 >>: 2 >>: 3 >>: MyEmpty
+
+
 }
